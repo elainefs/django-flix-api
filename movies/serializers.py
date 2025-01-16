1cbcd81a9b18a8790a59f916422eaa3b233a0b79
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-01-16 12:01:53 -0300

    feat: add ratings and validations for movies.

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,32 @@
+from django.db.models import Avg
 from rest_framework import serializers
+
 from .models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = "__all__"
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg("stars"))["stars__avg"]
+
+        if rate:
+            return round(rate, 1)
+
+        return None
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError("Release date cannot be before 1900")
+        return value
+
+    def validade_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError(
+                "Resume must not be longer than 500 characters"
+            )
+        return value
