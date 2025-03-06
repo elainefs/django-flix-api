89a9d91e319e9ec2215eccf55aba0cd5369c0995
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-03-06 17:16:21 -0300

    feat: set global permissions for permission_classes

--- actors/views.py
+++ actors/views.py
@@ -1,17 +1,19 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+from app.permissions import GlobalDefaultPermission
+
 from .models import Actor
 from .serializers import ActorSerializer
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaultPermission]
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, GlobalDefaultPermission]
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
--- genres/views.py
+++ genres/views.py
@@ -1,18 +1,19 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+from app.permissions import GlobalDefaultPermission
+
 from .models import Genre
-from .permissions import GenrePermissionClass
 from .serializers import GenreSerializer
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated, GenrePermissionClass]
+    permission_classes = [IsAuthenticated, GlobalDefaultPermission]
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, GenrePermissionClass]
+    permission_classes = [IsAuthenticated, GlobalDefaultPermission]
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
